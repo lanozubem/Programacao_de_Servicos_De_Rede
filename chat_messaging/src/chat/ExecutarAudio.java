2cc27c239225b387b464b818c5c979c92bb38816
Luano Zubém <34080784+lanozubem@users.noreply.github.com>
2020-04-22 20:29:06 -0300

    Update ExecutarAudio.java

--- chat_messaging/src/chat/ExecutarAudio.java
+++ chat_messaging/src/chat/ExecutarAudio.java
@@ -5,20 +5,20 @@ import java.net.URL;
 import javax.swing.*;
 
 public class ExecutarAudio extends JFrame{
-    JButton btnTocar = new JButton();
-    
+   
     public ExecutarAudio(){
+        JButton btnTocar = new JButton();
+        add(btnTocar);
+        
         setIconImage(new javax.swing.ImageIcon(getClass().getResource("/icons/bate-papo.png")).getImage());
-        btnTocar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/toWave.gif")));
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setSize(392, 312);
         setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);
         setAlwaysOnTop(true);
-        Boolean pausar = true;
-        add(btnTocar);
-
+    
+        btnTocar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/toWave.gif")));
         btnTocar.addFocusListener(new java.awt.event.FocusAdapter() {
             @Override
             public void focusGained(java.awt.event.FocusEvent evt) {
